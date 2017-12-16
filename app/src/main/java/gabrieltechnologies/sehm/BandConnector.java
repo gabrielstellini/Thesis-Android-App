37ce4719fa54244d84b4e5ced5cd6eb531fa7087
Gabriel Stellini <gs703@live.mdx.ac.uk>
2017-12-16 19:20:36 +0100

    Add bandConnector

---
+++ app/src/main/java/gabrieltechnologies/sehm/BandConnector.java
@@ -0,0 +1,24 @@
+package gabrieltechnologies.sehm;
+
+//Band imports
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+
+//Async import
+import android.os.AsyncTask;
+
+public class BandConnector extends AsyncTask<Void, Void, Void>{
+    private BandClient client = null;
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
