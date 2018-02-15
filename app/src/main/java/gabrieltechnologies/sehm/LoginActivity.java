4b9ef7dae0f773fb22d6785a2d05b2aa258078c2
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-15 23:56:52 +0100

    Add Auth0 screen

---
+++ app/src/main/java/gabrieltechnologies/sehm/LoginActivity.java
@@ -0,0 +1,143 @@
+package gabrieltechnologies.sehm;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class LoginActivity extends Activity {
+
+    private static final String API_URL = "http://192.168.4.194:3010/user/";
+    private static final String API_IDENTIFIER = "Android";
+
+    private String accessToken;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        Button callAPIWithTokenButton = (Button) findViewById(R.id.callAPIWithTokenButton);
+        Button callAPIWithoutTokenButton = (Button) findViewById(R.id.callAPIWithoutTokenButton);
+        Button loginWithTokenButton = (Button) findViewById(R.id.loginButton);
+        callAPIWithTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callAPI(true);
+            }
+        });
+        callAPIWithoutTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callAPI(false);
+            }
+        });
+        loginWithTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void callAPI(boolean sendToken) {
+        final Request.Builder reqBuilder = new Request.Builder()
+                .get()
+                .url(API_URL);
+        if (sendToken) {
+            reqBuilder.addHeader("Authorization", "Bearer " + accessToken);
+        }
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = reqBuilder.build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.isSuccessful()) {
+
+
+                            try {
+                                System.out.println("HERE " + response.body().string());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(LoginActivity.this, "API call success!", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, "API call failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void login() {
+        Auth0 auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
+        WebAuthProvider.init(auth0)
+                .withScheme("demo")
+                .withAudience(API_IDENTIFIER)
+                .withScope("openid profile email")
+                .start(LoginActivity.this, new AuthCallback() {
+                    @Override
+                    public void onFailure(@NonNull final Dialog dialog) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(final AuthenticationException exception) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull final Credentials credentials) {
+                        accessToken = credentials.getAccessToken();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Log in: Success", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+    }
+}
