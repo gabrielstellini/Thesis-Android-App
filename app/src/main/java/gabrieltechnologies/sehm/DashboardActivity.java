b984a912ce5adb6b18c0ee23b6d5dfbd785011d7
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-21 15:56:44 +0100

    Refactor dashboard

--- app/src/main/java/gabrieltechnologies/sehm/DashboardActivity.java
+++ app/src/main/java/gabrieltechnologies/sehm/DashboardActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Dashboard extends AppCompatActivity
+public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
